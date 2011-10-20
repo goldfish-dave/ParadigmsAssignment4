bfd4f0876e598e37be78961b1b6954cc2feac420
Ye; David <Ye; David daye4523@ug.it.usyd.edu.au>
2011-10-20 20:11:42 +1100

    cfg workig

--- task1-Control_Flow_Graph.py
+++ task1-Control_Flow_Graph.py
@@ -35,7 +35,7 @@ def parse(code): #takes in code, outputs the control flow graph in dictionary fo
 				if lengthBefore < len(tokens[token]): #checks if list has actually been changed
 					changes = True
 
-			elif successor[-1] == ':': #special case when a label follows another label
+			elif successor[-1] == ':' or successor.split()[1] == 'goto': #if a label or a 'goto' statement is the successor
 				lengthBefore = len(tokens[token])
 				tokens[token] += tokens[successor]
 				tokens[token] = list(set(tokens[token])) #uniques the list
@@ -65,22 +65,29 @@ def parse(code): #takes in code, outputs the control flow graph in dictionary fo
 	for x in sorted(list(tokens)):
 		print x, '-->', tokens[x]
 	"""
-def toDotFormat(tokens):
-	print """
+	return tokens
+
+def toDotFormat(tokens): #converts the dictionary form cfg to DOT langauge
+	print """ 
 /*
 * @command = dot
 *
 */
 digraph g {
-rankdir = LR
-a -> b ;
-a -> c ;
-a -> d ;
-a -> e;
-"a = 2" -> f;
-f -> a;
-} 
-	"""
+rankdir = LR"""
+	
+	#The format is as follows : <line in which the code appeared> : <code> 
+	for token in tokens:
+		if token[-1] != ':' and token.split()[1] != 'goto': #ignores labels and goto's
+			for successors in tokens[token]:
+				if successors.split()[1] != 'EOF': #ignores EOF
+					print	('"' 
+							+ token.split()[0] + ': ' + ' '.join(token.split()[1:])
+							+ '" -> "' 
+							+ successors.split()[0] + ': ' + ' '.join(successors.split()[1:])
+							+ '"'
+							)
+	print "}"
 
 if __name__ == "__main__":
 	toDotFormat(parse(testinput))
